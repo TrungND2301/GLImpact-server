a912d219a9f61e0a8819d23dc2314c2f6782f038
Trung <trung.nguyenduc2@gameloft.com>
2021-12-01 15:57:40 +0700

    ldap connect

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const http = require("http");
-const ldap = require("ldapjs");
+
+const { connect } = require("./ldap-communication");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -10,26 +12,11 @@ const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-const client = ldap.createClient({
-  url: "ldap://han-dc01.gameloft.org:389",
-  connectTimeout: 30000,
-  reconnect: true,
-});
-
-try {
-  client.bind("trung.nguyenduc2@gameloft.com", "byebye@2021");
-} catch (e) {
-  console.log("Bind failed");
-}
-
-if (client.connected == true) {
-  console.log("Connected");
-}
-
-client.on("error", (err) => {
-  console.log(err);
+server.listen(port, hostname, async () => {
+  try {
+    await connect();
+    console.log(`Listening to request on http://${hostname}:${port}/`);
+  } catch (error) {
+    console.log(error);
+  }
 });
--- ldap-communication.js
+++ ldap-communication.js
@@ -1,1 +1,77 @@
-import ldap from "ldapjs";
+const ldap = require("ldapjs");
+
+const { LDAP_URL } = process.env;
+
+let ldapClient;
+
+function authUser(client, username, password) {
+  return new Promise((resolve, reject) => {
+    client.bind(username, password, (err) => {
+      if (err) {
+        reject(new Error("Authentication failed!"));
+      } else {
+        resolve(client);
+        console.log("trung2");
+      }
+    });
+  });
+}
+
+function connect() {
+  return new Promise((resolve, reject) => {
+    if (!ldapClient) {
+      ldapClient = ldap.createClient({
+        url: [LDAP_URL],
+        reconnect: true,
+      });
+      ldapClient.on("connect", async (res) => {
+        // const user = getCredential();
+        // It can fetch user details
+        console.log("trung");
+        if (true) {
+          await authUser(
+            ldapClient,
+            "trung.nguyenduc2@gameloft.com",
+            "byebye@20211"
+          );
+        }
+        searchUser(ldapClient);
+        resolve(res);
+      });
+      ldapClient.on("connectError", (error) => {
+        reject(error);
+      });
+    } else {
+      reject(new Error("LDAP client already connected"));
+    }
+  });
+}
+
+function searchUser(client) {
+  return new Promise((resolve, reject) => {
+    client.search("dc=gameloft,dc=com", (err, res) => {
+      if (err) {
+        reject(new Error("Search failed!"));
+      } else {
+        resolve(client);
+        console.log("trung3");
+      }
+      res.on("searchRequest", (searchRequest) => {
+        console.log("searchRequest: ", searchRequest.messageID);
+      });
+      res.on("searchEntry", (entry) => {
+        console.log("entry: " + JSON.stringify(entry.object));
+      });
+      res.on("searchReference", (referral) => {
+        console.log("referral: " + referral.uris.join());
+      });
+      res.on("error", (err) => {
+        console.error("error: " + err.message);
+      });
+      res.on("end", (result) => {
+        console.log("status: " + result.status);
+      });
+    });
+  });
+}
+module.exports = { connect };
